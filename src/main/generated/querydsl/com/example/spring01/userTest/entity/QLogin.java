4be9fb110f3810dc06cefe8ad465957b321c1527
StyleBlue <cyberstar@outlook.com>
2023-05-11 16:56:26 +0900

    로그인 성공후 /user/test 진입

---
+++ src/main/generated/querydsl/com/example/spring01/userTest/entity/QLogin.java
@@ -0,0 +1,39 @@
+package com.example.spring01.userTest.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QLogin is a Querydsl query type for Login
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QLogin extends EntityPathBase<Login> {
+
+    private static final long serialVersionUID = -110854720L;
+
+    public static final QLogin login = new QLogin("login");
+
+    public final StringPath password = createString("password");
+
+    public final StringPath userId = createString("userId");
+
+    public QLogin(String variable) {
+        super(Login.class, forVariable(variable));
+    }
+
+    public QLogin(Path<? extends Login> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QLogin(PathMetadata metadata) {
+        super(Login.class, metadata);
+    }
+
+}
+
